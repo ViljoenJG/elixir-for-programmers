6883579c39fbb71a5e2d8c4cf619b1916b80d304
Kobus Viljoen <kobusv@icetech.io>
2022-01-07 16:13:54 +0200

    Added dictionary logic

--- hangman/dictionary/lib/dictionary.ex
+++ hangman/dictionary/lib/dictionary.ex
@@ -1,5 +1,9 @@
 defmodule Dictionary do
-  def hello do
-    :world
+  @word_list "assets/words.txt"
+             |> File.read!()
+             |> String.split(~r/\n/, trim: true)
+
+  def random_word do
+    Enum.random(@word_list)
   end
 end
